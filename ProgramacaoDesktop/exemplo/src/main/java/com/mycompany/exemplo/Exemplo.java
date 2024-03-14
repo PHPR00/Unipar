29fc77218e7da6db9f5b5b4cc2425e4f9eb24a50
Aluno UNIPAR <aluno@ac-cas.unipar.br>
2024-03-13 21:51:43 -0300

    #3 exemplo tela

---
+++ ProgramacaoDesktop/exemplo/src/main/java/com/mycompany/exemplo/Exemplo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new NewJFrame().setVisible(true);
+    }
+}
