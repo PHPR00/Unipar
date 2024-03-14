a8cc439c3bc5183c75a0517af7334263836c2321
Aluno UNIPAR <aluno@ac-cas.unipar.br>
2024-03-13 22:21:04 -0300

    desenvolvimento calculadora

---
+++ ProgramacaoDesktop/calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author aluno
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        new FrameCalculadora().setVisible(true);
+    }
+}
