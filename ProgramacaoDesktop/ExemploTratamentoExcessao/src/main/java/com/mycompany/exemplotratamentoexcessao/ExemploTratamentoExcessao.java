aa1de09018bbd31ab581da61ff43ee6db0c8d36e
Aluno UNIPAR <aluno@ac-cas.unipar.br>
2024-03-20 22:21:29 -0300

    tratamentoDeExeceoes

---
+++ ProgramacaoDesktop/ExemploTratamentoExcessao/src/main/java/com/mycompany/exemplotratamentoexcessao/ExemploTratamentoExcessao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplotratamentoexcessao;
+
+/**
+ *
+ * @author aluno
+ */
+public class ExemploTratamentoExcessao {
+
+    public static void main(String[] args) {
+         new FramePrincipal().setVisible(true);
+    }
+}
